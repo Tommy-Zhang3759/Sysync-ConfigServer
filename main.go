670bac0ba0b2d23fb3fa0519997ed50510ff9bb9
PelerYuan <pelerlittlepig@gmail.com>
2024-09-26 20:28:11 +0800

    fix main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"ConfigServer/utils"
-	"fmt"
+	"ConfigServer/webUI"
+	"time"
 )
 
 func init() {
@@ -12,44 +12,11 @@ func init() {
 func main() {
 	//var mvg sync.WaitGroup
 
-	//go webUI.StartServer("8080", webUI.Handler)
-	//
-	////mvg.Wait()
-	//// var clients []clientManage.Client
-	//for {
-	//	time.Sleep(time.Hour)
-	//}
+	go webUI.StartServer("8080", webUI.Handler)
 
-	csv, err := utils.OpenCSV("./test/example.csv")
-	if err != nil {
-		println(err.Error())
-		return
-	}
-	fmt.Println(csv.GetAllData())
-	fmt.Println(csv.GetRowData(1))
-	fmt.Println(csv.GetRowData(2))
-	fmt.Println(csv.GetRowData(4))
-	fmt.Println(csv.GetCellData("Name", 1))
-	fmt.Println(csv.GetCellData("Age", 2))
-	fmt.Println(csv.GetCellData("City", 3))
-	err = csv.SetRowData(1, []string{"Alice", "25", "New York"})
-	if err != nil {
-		return
-	}
-	err = csv.SetCellData("Name", 2, "Peler")
-	if err != nil {
-		return
-	}
-	fmt.Println(csv.GetAllData())
-	fmt.Println(csv.GetRowData(1))
-	fmt.Println(csv.GetRowData(2))
-	fmt.Println(csv.GetRowData(3))
-	fmt.Println(csv.GetCellData("Name", 1))
-	fmt.Println(csv.GetCellData("Age", 2))
-	fmt.Println(csv.GetCellData("City", 3))
-	err = csv.SaveCSV()
-	if err != nil {
-		println(err.Error())
-		return
+	//mvg.Wait()
+	// var clients []clientManage.Client
+	for {
+		time.Sleep(time.Hour)
 	}
 }
