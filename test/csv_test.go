68579db3749b131ad7374fdf7ba963fcc0eff903
PelerYuan <pelerlittlepig@gmail.com>
2024-09-30 17:42:04 +0800

    complete csv test file

--- test/csv_test.go
+++ test/csv_test.go
@@ -1,1 +1,44 @@
 package test
+
+import (
+	"ConfigServer/utils"
+	"fmt"
+	"testing"
+)
+
+func TestCSV(t *testing.T) {
+	csv_, err := utils.OpenCSV("./example.csv")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	fmt.Println(csv_.GetAllData())
+	fmt.Println(csv_.GetRowData(1))
+	fmt.Println(csv_.GetRowData(2))
+	fmt.Println(csv_.GetRowData(4)) // make an error
+	fmt.Println(csv_.GetCellData("Name", 1))
+	fmt.Println(csv_.GetCellData("Age", 2))
+	fmt.Println(csv_.GetCellData("City", 3))
+	err = csv_.SetRowData(1, []string{"Alice", "25", "New York"})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	err = csv_.SetCellData("Name", 2, "Peler")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	fmt.Println(csv_.GetAllData())
+	fmt.Println(csv_.GetRowData(1))
+	fmt.Println(csv_.GetRowData(2))
+	fmt.Println(csv_.GetRowData(3))
+	fmt.Println(csv_.GetCellData("Name", 1))
+	fmt.Println(csv_.GetCellData("Age", 2))
+	fmt.Println(csv_.GetCellData("City", 3))
+	err = csv_.SaveCSV()
+	if err != nil {
+		println(err.Error())
+		return
+	}
+}
